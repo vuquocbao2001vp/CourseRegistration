94e2788e00fd647db46cb1f05f5d84a868982a2b
vuquocbao2001vp <vuquocbao2001vp@gmail.com>
2023-04-22 01:16:16 +0700

    export-to-pdf

--- src/pages/RegistrationResultPage.vue
+++ src/pages/RegistrationResultPage.vue
@@ -1,73 +1,102 @@
 <template>
-  <div class="transcript-page">
-    <div class="transcript-header flex">
-      <div class="header-title font-semibold">KẾT QUẢ ĐĂNG KÝ HỌC</div>
-      <div class="student-info flex">
-        <div class="info-text font-semibold">Sinh viên: {{student.FullName}}</div>
-        <div class="info-text font-semibold">Mã số: {{student.StudentCode}}</div>
-        <div class="info-text font-semibold">Lớp: {{student.Class}}</div>
-        <div class="info-text font-semibold">Học kì: HK1-2024</div>
+  <div class="export-button" @click="exportToPdf">
+    <BaseButton buttonName="Xuất ra pdf" buttonType="white-square" />
+  </div>
+  <div id="result">
+    <div class="transcript-page">
+      <div class="transcript-header flex">
+        <div class="header-title font-semibold">KẾT QUẢ ĐĂNG KÝ HỌC</div>
+
+        <div class="student-info flex">
+          <div class="info-text font-semibold">
+            Sinh viên: {{ student.FullName }}
+          </div>
+          <div class="info-text font-semibold">
+            Mã số: {{ student.StudentCode }}
+          </div>
+          <div class="info-text font-semibold">Lớp: {{ student.Class }}</div>
+          <div class="info-text font-semibold">Học kì: HK1-2024</div>
+        </div>
       </div>
-    </div>
-    <div class="transcript-table">
-      <base-table>
-        <template #table-header>
-          <tr>
-            <th style="width: 40px">STT</th>
-            <th class="td-text-left" style="width: 140px">Mã môn học</th>
-            <th class="td-text-left" style="width: 200px">Tên môn học</th>
-            <th style="width: 40px">Số tín chỉ</th>
-            <th style="width: 120px">Trạng thái</th>
-            <th style="width: 120px">Thứ</th>
-            <th style="width: 120px">Tiết</th>
-            <th style="width: 120px">Giảng đường</th>
-          </tr>
-        </template>
-        <template #table-body>
-          <tr v-for="(subject, index) in enrollment" :key="index">
-            <td class="td-text-center">{{index + 1}}</td>
-            <td>{{subject.CourseCode}}</td>
-            <td>{{subject.CourseName}}</td>
-            <td class="td-text-center">{{subject.Credit}}</td>
-            <td class="td-text-center">{{regisStatus[subject.Status-1]}}</td>
-            <td class="td-text-center">{{subject.Schedule.substring(0, 5)}}</td>
-            <td class="td-text-center">{{subject.Schedule.substring(subject.Schedule.indexOf('(') + 1, subject.Schedule.indexOf(')')).replace('Tiết ', '')}}</td>
-            <td class="td-text-center">{{subject.Amphitheater}}</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td class="td-text-center font-semibold">Tổng số</td>
-            <td class="td-text-center">{{totalCredit}}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </template>
-      </base-table>
+      <div class="transcript-table">
+        <base-table>
+          <template #table-header>
+            <tr>
+              <th style="width: 40px">STT</th>
+              <th class="td-text-left" style="width: 140px">Mã môn học</th>
+              <th class="td-text-left" style="width: 200px">Tên môn học</th>
+              <th style="width: 40px">Số tín chỉ</th>
+              <th style="width: 120px">Trạng thái</th>
+              <th style="width: 120px">Thứ</th>
+              <th style="width: 120px">Tiết</th>
+              <th style="width: 120px">Giảng đường</th>
+            </tr>
+          </template>
+          <template #table-body>
+            <tr v-for="(subject, index) in enrollment" :key="index">
+              <td class="td-text-center">{{ index + 1 }}</td>
+              <td>{{ subject.CourseCode }}</td>
+              <td>{{ subject.CourseName }}</td>
+              <td class="td-text-center">{{ subject.Credit }}</td>
+              <td class="td-text-center">
+                {{ regisStatus[subject.Status - 1] }}
+              </td>
+              <td class="td-text-center">
+                {{ subject.Schedule.substring(0, 5) }}
+              </td>
+              <td class="td-text-center">
+                {{
+                  subject.Schedule.substring(
+                    subject.Schedule.indexOf("(") + 1,
+                    subject.Schedule.indexOf(")")
+                  ).replace("Tiết ", "")
+                }}
+              </td>
+              <td class="td-text-center">{{ subject.Amphitheater }}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td class="td-text-center font-semibold">Tổng số</td>
+              <td class="td-text-center">{{ totalCredit }}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </template>
+        </base-table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import BaseTable from '@/components/base/BaseTable.vue';
-import {mapGetters, mapMutations, mapActions} from 'vuex'
+import BaseTable from "@/components/base/BaseTable.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
 export default {
   components: { BaseTable },
   data() {
     return {
-      regisStatus: ['Đăng ký lần đầu', 'Học lại', 'Học cải thiện', 'Đăng ký tự do'],
-    }
+      regisStatus: [
+        "Đăng ký lần đầu",
+        "Học lại",
+        "Học cải thiện",
+        "Đăng ký tự do",
+      ],
+    };
   },
   computed: {
     ...mapGetters(["enrollments", "student"]),
-    enrollment(){
-      if(this.enrollments) return this.enrollments.enrollment;
+    enrollment() {
+      if (this.enrollments) return this.enrollments.enrollment;
       else return [];
     },
-    totalCredit(){
-      if(this.enrollments) return this.enrollments.totalCredit;
+    totalCredit() {
+      if (this.enrollments) return this.enrollments.totalCredit;
       else return 0;
     },
   },
@@ -75,14 +104,34 @@ export default {
     const info = JSON.parse(localStorage.getItem("info"));
     if (info) {
       this.setStudent(info);
-      this.getEnrollments(info.StudentID)
+      this.getEnrollments(info.StudentID);
     } else {
       this.setStudent({});
     }
   },
   methods: {
     ...mapMutations(["setEnrollments", "setStudent"]),
-    ...mapActions(["getEnrollments"])
+    ...mapActions(["getEnrollments"]),
+
+    exportToPdf() {
+      const element = document.getElementById("result");
+      html2canvas(element, {
+        allowTaint: true,
+        useCORS: true,
+        scale: 2,
+      }).then((canvas) => {
+        const pdfWidth = 210;
+        const pdfHeight = 297;
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", [pdfWidth, pdfHeight]);
+        const imgProps = pdf.getImageProperties(imgData);
+        const imgWidth = 200;
+        const imgHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        const x = 5;
+        pdf.addImage(imgData, "PNG", x, 5, imgWidth, imgHeight);
+        pdf.save("KetQuaDangKyHoc.pdf");
+      });
+    },
   },
 };
 </script>
@@ -99,18 +148,24 @@ export default {
 }
 .info-text {
   font-size: 16px;
-  padding: 16px 40px;
+  padding: 16px 20px;
+  white-space: nowrap;
 }
-.transcript-table{
+.transcript-table {
   width: 100%;
   height: calc(100vh - 225px);
   overflow: auto;
   box-sizing: border-box;
+  position: relative;
 }
-
 
 tbody tr:hover {
   background-color: #fff;
 }
-
+.export-button {
+  position: absolute;
+  right: 20px;
+  width: 140px;
+  top: 80px;
+}
 </style>
--- src/pages/TranscriptPage.vue
+++ src/pages/TranscriptPage.vue
@@ -1,78 +1,94 @@
 <template>
-  <div class="transcript-page">
-    <div class="transcript-header flex">
-      <div class="header-title font-semibold">KẾT QUẢ HỌC TẬP</div>
-      <div class="student-info flex">
-        <div class="info-text font-semibold">Sinh viên: {{student.FullName}}</div>
-        <div class="info-text font-semibold">Mã số: {{student.StudentCode}}</div>
-        <div class="info-text font-semibold">Lớp: {{student.Class}}</div>
+  <div id="transcript">
+    <div class="transcript-page">
+      <div class="transcript-header flex">
+        <div class="header-title font-semibold">KẾT QUẢ HỌC TẬP</div>
+        <div class="student-info flex">
+          <div class="info-text font-semibold">
+            Sinh viên: {{ student.FullName }}
+          </div>
+          <div class="info-text font-semibold">
+            Mã số: {{ student.StudentCode }}
+          </div>
+          <div class="info-text font-semibold">Lớp: {{ student.Class }}</div>
+        </div>
+      </div>
+      <div class="transcript-table">
+        <table id="my-table">
+          <thead>
+            <tr>
+              <th class="td-text-left" style="width: 40px">Học kì</th>
+              <th style="width: 40px">STT</th>
+              <th class="td-text-left" style="width: 140px">Mã môn học</th>
+              <th class="td-text-left" style="width: 200px">Tên môn học</th>
+              <th style="width: 120px">Số tín chỉ</th>
+              <th style="width: 120px">Điểm</th>
+            </tr>
+          </thead>
+          <tbody>
+            <template v-for="(course, index) in transcript" :key="index">
+              <tr>
+                <td :rowspan="course.Grades.length">{{ course.Semester }}</td>
+                <td class="td-text-center">1</td>
+                <td>{{ course.Grades[0].CourseCode }}</td>
+                <td>{{ course.Grades[0].CourseName }}</td>
+                <td class="td-text-center">{{ course.Grades[0].Credit }}</td>
+                <td class="td-text-center">{{ course.Grades[0].Score }}</td>
+              </tr>
+              <template
+                v-for="(grade, gradeIndex) in course.Grades.slice(1)"
+                :key="gradeIndex"
+              >
+                <tr>
+                  <td class="td-text-center">{{ gradeIndex + 2 }}</td>
+                  <td>{{ grade.CourseCode }}</td>
+                  <td>{{ grade.CourseName }}</td>
+                  <td class="td-text-center">{{ grade.Credit }}</td>
+                  <td class="td-text-center">{{ grade.Score }}</td>
+                </tr>
+              </template>
+            </template>
+          </tbody>
+        </table>
+      </div>
+      <div class="transcript-total">
+        <div class="transcript-total-title">
+          Tổng tín chỉ: {{ totalCredit }}
+        </div>
+        <div class="transcript-total-title">
+          Số tín chỉ tích lũy: {{ accumulatedCredit }}
+        </div>
+        <div class="transcript-total-title">
+          Điểm trung bình: {{ accumulatedScore }}
+        </div>
       </div>
     </div>
-    <div class="transcript-table">
-      <table>
-        <thead>
-          <tr>
-            <th class="td-text-left" style="width: 40px">Học kì</th>
-            <th style="width: 40px">STT</th>
-            <th class="td-text-left" style="width: 140px">Mã môn học</th>
-            <th class="td-text-left" style="width: 200px">Tên môn học</th>
-            <th style="width: 120px">Số tín chỉ</th>
-            <th style="width: 120px">Điểm</th>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="(course, index) in transcript" :key="index">
-            <tr>
-              <td :rowspan="course.Grades.length">{{ course.Semester }}</td>
-              <td class="td-text-center">1</td>
-              <td>{{ course.Grades[0].CourseCode }}</td>
-              <td>{{ course.Grades[0].CourseName }}</td>
-              <td class="td-text-center">{{ course.Grades[0].Credit }}</td>
-              <td class="td-text-center">{{ course.Grades[0].Score }}</td>
-            </tr>
-            <template
-              v-for="(grade, gradeIndex) in course.Grades.slice(1)"
-              :key="gradeIndex"
-            >
-              <tr>
-                <td class="td-text-center">{{ gradeIndex + 2 }}</td>
-                <td>{{ grade.CourseCode }}</td>
-                <td>{{ grade.CourseName }}</td>
-                <td class="td-text-center">{{ grade.Credit }}</td>
-                <td class="td-text-center">{{ grade.Score }}</td>
-              </tr>
-            </template>
-          </template>
-        </tbody>
-      </table>
-    </div>
-    <div class="transcript-total">
-      <div class="transcript-total-title">Tổng tín chỉ: {{totalCredit}}</div>
-      <div class="transcript-total-title">Số tín chỉ tích lũy: {{accumulatedCredit}}</div>
-      <div class="transcript-total-title">Điểm trung bình: {{accumulatedScore}}</div>
-    </div>
   </div>
+  <!-- <div class="export-button" @click="exportToPdf">
+    <BaseButton buttonName="Xuất ra pdf" buttonType="white-square" />
+  </div> -->
 </template>
 
 <script>
-import {mapGetters, mapMutations, mapActions} from 'vuex'
+import { mapGetters, mapMutations, mapActions } from "vuex";
+
 export default {
   computed: {
     ...mapGetters(["transcripts", "student"]),
-    transcript(){
-      if(this.transcripts) return this.transcripts.transcript;
+    transcript() {
+      if (this.transcripts) return this.transcripts.transcript;
       else return [];
     },
-    totalCredit(){
-      if(this.transcripts) return this.transcripts.totalCredit;
+    totalCredit() {
+      if (this.transcripts) return this.transcripts.totalCredit;
       else return 0;
     },
-    accumulatedCredit(){
-      if(this.transcripts) return this.transcripts.accumulatedCredit;
+    accumulatedCredit() {
+      if (this.transcripts) return this.transcripts.accumulatedCredit;
       else return 0;
     },
-    accumulatedScore(){
-      if(this.transcripts) return this.transcripts.accumulatedScore;
+    accumulatedScore() {
+      if (this.transcripts) return this.transcripts.accumulatedScore;
       else return 0;
     },
   },
@@ -80,14 +96,14 @@ export default {
     const info = JSON.parse(localStorage.getItem("info"));
     if (info) {
       this.setStudent(info);
-      this.getTranscipts(info.StudentID)
+      this.getTranscipts(info.StudentID);
     } else {
       this.setStudent({});
     }
   },
   methods: {
     ...mapMutations(["setTranscripts", "setStudent"]),
-    ...mapActions(["getTranscipts"])
+    ...mapActions(["getTranscipts"]),
   },
 };
 </script>
@@ -104,19 +120,20 @@ export default {
 }
 .info-text {
   font-size: 16px;
-  padding: 16px 40px;
+  padding: 16px 20px;
+  white-space: nowrap;
 }
-.transcript-table{
+.transcript-table {
   width: 100%;
   height: calc(100vh - 297px);
   overflow: auto;
   box-sizing: border-box;
   border-bottom: 1px solid #ccc;
 }
-.transcript-total{
+.transcript-total {
   padding-top: 16px;
 }
-.transcript-total-title{
+.transcript-total-title {
   font-size: 14px;
   padding: 3px 0;
 }
@@ -167,5 +184,11 @@ th {
 }
 .td-text-right {
   text-align: right;
+}
+.export-button {
+  position: absolute;
+  right: 20px;
+  width: 140px;
+  top: 80px;
 }
 </style>
